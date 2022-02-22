b7d9b8c05f9a6e11f1a801c340e98bbd62ea1aac
Luis Zamora <lzamora@espol.edu.ec>
2022-02-21 23:10:28 -0500

    changes for function

---
+++ script.py
@@ -0,0 +1,45 @@
+
+def processOverlap(range_1,range_2):
+    return (range_1[0]<= range_2[1]) and (range_2[0]<range_1[1])
+
+def convertToSeconds(time):
+    hour,minutes = time.strip().split(":")
+    hour_seconds = int(hour) * 3600
+    minutes_seconds = int(minutes) * 60 
+    return hour_seconds + minutes_seconds
+
+def processFile(file):
+    schedules_dic ={}
+    names_dic = {}
+    names_list = []
+
+    #build the main dic
+    for row in file:
+        name,schedules = row.strip().split("=")
+        schedules = schedules.split(",")
+        for schedule in schedules:
+            day = schedule[:2]
+            time_range = schedule[3:].split("-")
+            schedules_dic[day] = schedules_dic.get(day,{})
+            schedules_dic[day][name] = [convertToSeconds(i) for i in time_range] 
+        names_list.append(name)
+    
+    #proccess the dic 
+    
+    for value in schedules_dic.values():
+        if len(value)>1:
+            data_list = list(value.items())
+            for idx,(name_1,range_1) in enumerate(data_list):
+                for name_2,range_2 in data_list[idx + 1:]:
+                    if processOverlap(range_1,range_2):
+                        names_dic[(name_1,name_2)] = names_dic.get((name_1,name_2),0) +1
+
+    return names_dic
+
+
+
+file = open("resources/calendar3.csv","r")
+d = processFile(file)
+print()
+print(d)
+#pprint.pprint(d)
